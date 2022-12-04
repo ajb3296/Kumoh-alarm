8de6ba53d8ca66782d4a8e8b1b449d7280481e7a
An Jaebeom <ajb8533296@gmail.com>
2022-12-05 01:06:36 +0900

    정리

--- bot/background/broadcast.py
+++ bot/background/broadcast.py
@@ -11,37 +11,40 @@ async def broadcast(bot):
         await asyncio.sleep(5)
     latest_data_id = seBoardDB.get_latest_data_id()
     await asyncio.sleep(5)
+    
     while True:
         now_latest_data_id = seBoardDB.get_latest_data_id()
         if latest_data_id != now_latest_data_id:
-            for num in range(latest_data_id + 1, now_latest_data_id + 1):
+            for data_id in range(latest_data_id + 1, now_latest_data_id + 1):
                 # get post
-                post = seBoardDB.get_database_from_id(num)
+                post = seBoardDB.get_database_from_id(data_id)
                 try:
                     img_preview, preview = await get_preview(post[1])
                 except:
                     # 글 수정/삭제되었을 경우 오류 예외처리
                     preview = False
+    
+                # 메시지 전송
                 if post is not None:
                     LOGGER.info(f"Send msg : {post}")
                     await send_msg(bot, post, preview, img_preview)
-
+    
             latest_data_id = now_latest_data_id
         await asyncio.sleep(60)
 
 async def send_msg(bot, post, preview, img_preview):
+    # 빨간색
     if post[3] in ["오득환", "김선명", "이현아", "김시관", "신윤식", "이해연", "김병만", "전태수", "학과장"]:
-        # 빨간색
         color = 0xff0000
         important = ":red_circle: 매우 중요"
         everyone_ping = True
+    # 오렌지색
     elif post[3] in ["이한나[조교]", "학생회"]:
-        # 오렌지색
         color = 0xff7f00
         important = ":orange_circle: 중요"
         everyone_ping = False
+    # 초록색
     else:
-        # 초록색
         color = 0x008000
         important = ":green_circle: 보통"
         everyone_ping = False
@@ -51,14 +54,17 @@ async def send_msg(bot, post, preview, img_preview):
         for channel_id in channel_id_list:
             target_channel = bot.get_channel(channel_id)
             try:
+                # 중요도에 따라 everyone ping
                 if everyone_ping:
                     await target_channel.send("@everyone")
                 embed=discord.Embed(title=post[2], description=f"", color=color)
                 embed.add_field(name="글쓴이", value=post[3], inline=True)
                 embed.add_field(name="중요도", value=important, inline=True)
                 embed.add_field(name="링크", value=f"{se_board_link}freeboard/{post[1]}", inline=False)
+                # 미리보기 텍스트가 있을 경우
                 if preview is not False:
                     embed.add_field(name="미리보기", value=preview, inline=False)
+                # 이미지 미리보기가 있을 경우
                 if img_preview is not None:
                     embed.set_image(url=img_preview)
                 embed.set_footer(text=BOT_NAME_TAG_VER)
