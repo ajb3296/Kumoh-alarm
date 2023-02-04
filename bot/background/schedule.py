a4b59191d6f09ceea3eeacf3932d1d2c990c1f66
An Jaebeom <ajb8533296@gmail.com>
2023-02-04 23:03:27 +0900

    add next year's schedule, edit changed schedule

--- bot/background/schedule.py
+++ bot/background/schedule.py
@@ -2,6 +2,7 @@ import json
 import asyncio
 import datetime
 import traceback
+from dateutil.relativedelta import relativedelta
 
 from bot.utils.crawler import getText
 from bot.utils.database import channelDataDB
@@ -37,6 +38,7 @@ async def schedule(bot):
                                 # 서버에 등록된 스케쥴 가져오기
                                 schedules_in_server = get_server.scheduled_events
 
+                                # description이 일치하는 스케쥴 찾기
                                 server_data = None
                                 for sc in schedules_in_server:
                                     if sc.description == articleNo:
@@ -51,13 +53,28 @@ async def schedule(bot):
                                                                     location="금오공과대학교",
                                                                     start_time=start_time,
                                                                     end_time=end_time)
+                                # 서버에 스케쥴이 등록되어 있으면
+                                else:
+                                    # 변경점 확인
+                                    if server_data.name != title or server_data.start_time != start_time or server_data.end_time != end_time:
+                                        # 다를경우 수정
+                                        await server_data.edit(name=title,
+                                                        description=articleNo,
+                                                        location="금오공과대학교",
+                                                        start_time=start_time,
+                                                        end_time=end_time)
         await asyncio.sleep(3600)
 
 async def get_schedule() -> list:
     """ 금오공대 학사일정 가져오기 """
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'}
-    link = f"https://www.kumoh.ac.kr/app/common/selectDataList.do?sqlId=jw.Article.selectCalendarArticle&modelNm=list&jsonStr=%7B%22year%22%3A%22{datetime.datetime.now().year}%22%2C%22bachelorBoardNoList%22%3A%5B%2212%22%5D%7D"
-    result = await getText(link, header)
+    this_year_link = f"https://www.kumoh.ac.kr/app/common/selectDataList.do?sqlId=jw.Article.selectCalendarArticle&modelNm=list&jsonStr=%7B%22year%22%3A%22{datetime.datetime.now().year}%22%2C%22bachelorBoardNoList%22%3A%5B%2212%22%5D%7D"
+    next_year_link = f"https://www.kumoh.ac.kr/app/common/selectDataList.do?sqlId=jw.Article.selectCalendarArticle&modelNm=list&jsonStr=%7B%22year%22%3A%22{(datetime.datetime.now() + relativedelta(years=1)).year}%22%2C%22bachelorBoardNoList%22%3A%5B%2212%22%5D%7D"
 
-    result = json.loads(result)
-    return result['list']
+    this_year = await getText(this_year_link, header)
+    this_year = json.loads(this_year)['list']
+
+    next_year = await getText(next_year_link, header)
+    next_year = json.loads(next_year)['list']
+
+    return this_year + next_year
