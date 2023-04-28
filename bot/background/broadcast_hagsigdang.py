d30fe6e6ced156fcb38ad55a5aaeebb48b8729af
An Jaebeom <ajb8533296@gmail.com>
2023-04-28 22:12:36 +0900

    학생식당 조식, 중식 전부 표시되도록 변경

--- bot/background/broadcast_hagsigdang.py
+++ bot/background/broadcast_hagsigdang.py
@@ -15,16 +15,20 @@ async def broadcast_hagsigdang(bot) -> None:
 
     while True:
         if datetime.now().weekday() <= 4: # 월 ~ 금
-            if datetime.now().hour == 9 and datetime.now().minute == 0:
+            if datetime.now().hour == 7 and datetime.now().minute == 0:
                 result = await getText(link, header)
                 parse = BeautifulSoup(result, 'lxml')
                 box = parse.find("div", {"class": "menu-list-box"})
-                today_menu = box.find_all("td")[datetime.now().weekday()].get_text().strip()
 
-                await send_hagsigdang(bot, today_menu)
+                today_menu_list = []
+                for i in box.find("tbody").find_all("tr"):
+                    menu = i.find_all("td")[datetime.now().weekday()].get_text().strip().split("\n")
+                    today_menu_list.append([menu[0], '\n'.join(menu[1:]).strip()])
+
+                await send_hagsigdang(bot, today_menu_list)
         await asyncio.sleep(60)
 
-async def send_hagsigdang(bot, today_menu: str) -> None:
+async def send_hagsigdang(bot, today_menu: list(list(str, str))) -> None:
     """ 학생식당 메뉴 전송 """
     # 채널 아이디 리스트 가져오기
     channel_id_list = channelDataDB().get_on_channel("Hagsigdang")
@@ -33,7 +37,12 @@ async def send_hagsigdang(bot, today_menu: str) -> None:
         for channel_id in channel_id_list:
             target_channel = bot.get_channel(channel_id)
             try:
-                embed = discord.Embed(title="오늘의 학생식당 메뉴", description=today_menu, color=color_code)
+                embed = discord.Embed(title="오늘의 학생식당 메뉴", description='', color=color_code)
+
+                for menu in today_menu:
+                    menu_title, menu_content = menu
+                    embed.add_field(name=menu_title, value=menu_content, inline=False)
+
                 embed.set_footer(text=BOT_NAME_TAG_VER)
                 await target_channel.send(embed=embed)
 
